ed6ba509b7502d35117ce1cfe3d167ce3026607f
Ntethelelo Zikalala <Ntethelelo.Zikalala@fnb.co.za>
2024-02-03 13:52:57 +0200

    - Using inversion of control/dependency injection to make inventory sytem flexible to work with any type of list implementation.

--- src/altron/car/inventory/service/CarInventorySystem.java
+++ src/altron/car/inventory/service/CarInventorySystem.java
@@ -9,9 +9,13 @@ import java.util.Comparator;
 import java.util.List;
 
 public class CarInventorySystem {
-    private static List<Car> cars = new ArrayList<>();
+    private final List<Car> cars;
 
-    public static void addCar(String make, String model, int year, Integer batteryLife) {
+    public CarInventorySystem(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void addCar(String make, String model, int year, Integer batteryLife) {
         Car car;
         if (batteryLife == null) {
             car = new Car(make, model, year);
@@ -24,17 +28,17 @@ public class CarInventorySystem {
     //Generic method for adding ALL types of cars
     //This leaves the validation at the model creation level
     //Ensuring we don't modify the validation logic each time we add a different type of car
-    public static void addCar(Car car) {
+    public void addCar(Car car) {
         cars.add(car);
     }
 
-    public static void showCars() {
+    public void showCars() {
         for (Car car : cars) {
             System.out.println(car.displayInfo());
         }
     }
 
-    public static List<Car> searchCars(String make) {
+    public List<Car> searchCars(String make) {
         List<Car> result = new ArrayList<>();
         for (Car car : cars) {
             if (car.displayInfo().contains(make)) {
@@ -44,24 +48,26 @@ public class CarInventorySystem {
         return result;
     }
 
-    public static void sortCarsBy(String property) {
+    public void sortCarsBy(String property) {
         if ("year".equals(property)) {
             Collections.sort(cars, Comparator.comparingInt(Car::getYear));
         }
     }
 
     public static void main(String[] args) {
+
+        CarInventorySystem carInventorySystem = new CarInventorySystem(new ArrayList<>());
         // Example usage
-        addCar("Toyota", "Corolla", 2020, null);
-        addCar("Tesla", "Model S", 2022, 24);
-        addCar("Ford", "Mustang", 2021, null);
-        addCar("BMW", "M3", 2021, null);
-        addCar("BMW", "i8", 2021, 50);
-        addCar(new Car("Auid","A3",2020));
+        carInventorySystem.addCar("Toyota", "Corolla", 2020, null);
+        carInventorySystem.addCar("Tesla", "Model S", 2022, 24);
+        carInventorySystem.addCar("Ford", "Mustang", 2021, null);
+        carInventorySystem.addCar("BMW", "M3", 2021, null);
+        carInventorySystem.addCar("BMW", "i8", 2021, 50);
+        carInventorySystem.addCar(new Car("Auid","A3",2020));
 
         // Testing the functions
-        System.out.println(searchCars("Tesla"));
-        sortCarsBy("year");
-        showCars();
+        System.out.println(carInventorySystem.searchCars("Tesla"));
+        carInventorySystem.sortCarsBy("year");
+        carInventorySystem.showCars();
     }
 }
